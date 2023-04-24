7352897d3796ac294288ece29227d2f55793aa37
naborlozada <35967936+naborlozada@users.noreply.github.com>
2023-04-25 01:34:03 +0200

    Update pnps_ratio.R

--- selection/pnps_ratio.R
+++ selection/pnps_ratio.R
@@ -141,7 +141,7 @@ for (i in seq_along(TsTv_byGene)) {
 }
 
 
-# Method's Li (1993): improved Kimura's 2-parameter model
+# Li's Method (1993): improved Kimura's 2-parameter model
 main_results_table$pS = main_results_table$B4 + (((main_results_table$L2 * main_results_table$A2) + (main_results_table$L4*main_results_table$A4))/(main_results_table$L2 + main_results_table$L4))
 main_results_table$pN = main_results_table$A0 + (((main_results_table$L0 * main_results_table$B0) + (main_results_table$L2*main_results_table$B2))/(main_results_table$L0 + main_results_table$L2))
 # reference: J. Mol. Evol. 1993: 36,96-99. 
