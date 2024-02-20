e05f1fde525e23641e4c7aa8886f5a96bdfb9318
Nabor Lozada <35967936+naborlozada@users.noreply.github.com>
2024-02-20 13:11:18 +0100

    Update readme

--- selection/pnps_ratio.R
+++ selection/pnps_ratio.R
@@ -6,7 +6,7 @@
 #            nabor.lozada@gmail.com                #
 # ------------------------------------------------ #
 
-# Part of these scripting lines were obtained from the course "Computational Genomics GEN8900".
+# Part of these scripting lines were obtained from the course "Computational Genomics".
 # Additional modifications were written afterwards. 
 
 
